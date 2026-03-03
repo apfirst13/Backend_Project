fd1395264320c580e9e43e0d90422913b240a843
ap.first_13 <apiwatpan2549@gmail.com>
2026-03-03 18:04:12 +0700

    feat: add article detail view and update home page links

--- pj4_personalblog/personal-blog/server.js
+++ pj4_personalblog/personal-blog/server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const fs = require('fs'); // 👈 เพิ่มบรรทัดนี้: fs ย่อมาจาก File System เอาไว้อ่านไฟล์ในเครื่อง
+const fs = require('node:fs'); // เพิ่มบรรทัดนี้: fs ย่อมาจาก File System เอาไว้อ่านไฟล์ในเครื่อง
 
 const app = express();
 app.set('view engine', 'ejs');
@@ -13,6 +13,23 @@ app.get('/', (req, res) => {
     
     // 3. ส่งข้อมูลที่ชื่อว่า posts แนบไปกับไฟล์ home.ejs
     res.render('home', { posts: posts }); 
+});
+
+// ดักจับ Request ที่มีคำว่า /article/ ตามด้วยตัวเลขอะไรก็ได้ (:id)
+app.get('/article/:id', (req, res) => {
+    // 1. รับ ID จาก URL (เช่น ถ้าเข้า /article/1 ค่า req.params.id จะเท่ากับ 1)
+    const articleId = req.params.id;
+
+    // 2. เดินไปอ่านไฟล์ posts.json เหมือนเดิม
+    const rawData = fs.readFileSync('./data/posts.json');
+    const posts = JSON.parse(rawData);
+
+    // 3. ค้นหาบทความที่มี id ตรงกับที่ลูกค้าขอมา
+    // .find() เป็นคำสั่งของ JavaScript ไว้หาของใน List ครับ
+    const post = posts.find(p => p.id == articleId);
+
+    // 4. ส่งข้อมูลบทความที่หาเจอ ไปให้หน้า article.ejs แสดงผล
+    res.render('article', { post: post });
 });
 
 app.listen(3000, () => {
